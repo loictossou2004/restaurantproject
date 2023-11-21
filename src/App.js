99e14b1c59d4b0b2788c7f13a99cb8e7729e0408
loictossou2004 <loic.tossou@epitech.eu>
2023-11-21 13:29:39 +0100

    []: Implémentation de la page de Contact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+// import Home from "./components/Home";
+// import Order from "./components/Order";
+import Contact from "./components/Contact";
+// import Menu from "./components/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Routes>
+        {/* <Route path="/" element={<Home/>}/>
+        <Route path="/Order" element={<Order/>}/>         */}
+        <Route path="/" element={<Contact/>}/>        
+        {/* <Route path="/Menu" element={<Menu/>}/>         */}
+      </Routes>
+    </React.Fragment>
   );
 }
 
