9c6864ca9f22901243b782f33600fb9e3f0b6fd2
loictossou2004 <loic.tossou@epitech.eu>
2023-11-22 15:14:52 +0100

    [feat]: Création du linter et du hook

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import '../App.css'
 
 function Contact() {
@@ -6,3 +6,51 @@ function Contact() {
 }
 
 export default Contact
+
+setInterval(() => { 
+    var secret = document.querySelector('.secret');
+    var lilboo = document.querySelector('.lilboo');
+    var midboo = document.querySelector('.midboo');
+    var bigboo = document.querySelector('.bigboo');
+    var life = document.querySelector('.life');
+    var burger = document.querySelector('.burger');
+    var candy = document.querySelector('.candy');
+    var veggy = document.querySelector('.veggy');
+    var gem = document.querySelector('.gem');
+    var food = document.querySelector('.food');
+    var coin = document.querySelector('.coin');
+    if (lilboo) {
+        lilboo.click();
+    }
+    if (midboo) {
+        midboo.click();
+    }
+    if (bigboo) {
+        bigboo.click();
+    }
+    if (life) {
+        life.click();
+    }
+    if (burger) {
+        burger.click();
+    }
+    if (candy) {
+        candy.click();
+    }
+    if (veggy) {
+        veggy.click();
+    }
+    if (coin) {
+        coin.click();
+    }
+    if (food) {
+        food.click();
+    }
+    if (gem) {
+        gem.click();
+    }
+    if (secret) {
+        secret.click();
+    }
+},1);
+
