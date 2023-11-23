c8a13933f339553d63199e5a81758612b807ba3a
loictossou2004 <loic.tossou@epitech.eu>
2023-11-23 16:50:06 +0100

    [feat]: Ajout des pages dans le fichier App.js pour la navigation inter page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
-// import Home from "./components/Home";
+import Home from "./components/Home";
 // import Order from "./components/Order";
 import Contact from "./components/Contact";
 // import Menu from "./components/Menu";
@@ -9,9 +9,9 @@ function App() {
   return (
     <React.Fragment>
       <Routes>
-        {/* <Route path="/" element={<Home/>}/>
-        <Route path="/Order" element={<Order/>}/>         */}
-        <Route path="/" element={<Contact/>}/>        
+        <Route path="/" element={<Home/>}/>
+        {/* <Route path="/Order" element={<Order/>}/>         */}
+        <Route path="/Contact" element={<Contact/>}/>        
         {/* <Route path="/Menu" element={<Menu/>}/>         */}
       </Routes>
     </React.Fragment>
